84c27242407cf6ef3668d88dc237dd1c64b4d248
Martin Zaragoza <martin.zaragoza@klarna.com>
2021-12-20 22:48:11 +0100

    Refactoring AssetLoader to make more sense

--- src/mz/swhero/AssetLoader.js
+++ src/mz/swhero/AssetLoader.js
@@ -1,34 +1,25 @@
-/* Guarda todas las escenas existentes */
-const scenes = [];
-/* Guarda las claves de un set de animaciones */
-const animKeys = {};
+//@ts-check
 
-/**
- * Intenta cargar assets para una escena (en caso que no hayan sido cargados previamente.)
- * @param {Phaser.Scene} scene Objeto Phaser.Scene.
- * @param {String} key Clave de assets (ej: 'player')
- * @param {Function} loaderFunction Lambda encargada de cargar los assets.
- */
-function loadFor(scene, key, loaderFunction) {
-    const sceneIo = scenes.indexOf(scene);
-    const sceneFound = sceneIo >= 0;
-    if (!sceneFound) return loadNewSceneAssets(scene, key, loaderFunction);
+export default class AssetLoader {
+    static allAssets = {}
 
-    const keyFound = animKeys[sceneIo][key];
-    if (!keyFound) return loadAssets(scene, key, loaderFunction);
+    /**
+     * Intenta cargar assets para una escena (en caso que no hayan sido cargados previamente.)
+     * @param {Phaser.Scene} scene Objeto Phaser.Scene.
+     * @param {String} itemName Clave de assets (ej: 'player')
+     * @param {Function} loaderFunction Lambda encargada de cargar los assets.
+     */
+    static loadFor(scene, itemName, loaderFunction) {
+        const sceneKey = scene.scene.key
+        
+        const sceneIsMissing = AssetLoader.allAssets[sceneKey] === undefined
+        if (sceneIsMissing) { AssetLoader.allAssets[sceneKey] = {} }
+
+        const itemIsMissing = AssetLoader.allAssets?.[sceneKey]?.[itemName] === undefined
+        if (itemIsMissing) {
+            console.log('Loading item ', itemName, ' for scene ', sceneKey)
+            loaderFunction()
+            AssetLoader.allAssets[sceneKey][itemName] = true
+        }
+    }
 }
-
-function loadNewSceneAssets(scene, key, loaderFunction) {
-    const io = scenes.push(scene) - 1;
-    animKeys[io] = {};
-    animKeys[io][key] = true;
-    loaderFunction();
-}
-
-function loadAssets(scene, key, loaderFunction) {
-    const io = scenes.indexOf(scene);
-    animKeys[io][key] = true;
-    loaderFunction();
-}
-
-export default { loadFor };
